29f4e18e5bfe0643f7acd9555ec6af582bac2849
Kristiina Kumila <kristiinakumila@gmail.com>
2020-09-15 16:04:12 +0300

    changes

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const port = process.env.port || 3001
-app.listen(port, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${port}`)
 })
