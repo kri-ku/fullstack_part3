8698e392bc6c47f41ecda7a185a40a7f515e817c
Kristiina Kumila <kristiinakumila@gmail.com>
2020-09-25 14:43:17 +0300

    errrors

--- mongo.js
+++ mongo.js
@@ -5,10 +5,9 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+//const password = process.argv[2]
 
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0.ezxec.mongodb.net/phonebook?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
